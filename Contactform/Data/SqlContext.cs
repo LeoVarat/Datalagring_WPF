37d9c97c11d5a490761bfa430fd0ebe061bc09a9
Leo Varat <predragvarat@gmail.com>
2022-01-23 17:01:11 +0100

    Base for DB
    Base for DB created

---
+++ Contactform/Data/SqlContext.cs
@@ -0,0 +1,32 @@
+﻿using Contactform.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactform.Data
+{
+    internal class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<Adress> Adresses { get; set; }
+        public virtual DbSet<EventStatus> EventStatuses { get; set; }
+        public virtual DbSet<UserEvent> UserEvents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
